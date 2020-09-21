02322a48f35c7e7b3496b1715c35ae49d58369e0
Blake <telefragent@gmail.com>
2020-09-21 14:10:15 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public CharacterController CC;
+	public Transform CameraTransform;
+	public float MoveSpeed;
+	public float MouseSensitivity;
+
+	private float xRotation = 0f;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * MoveSpeed * Time.deltaTime;
+		CC.Move((moveInput.y * transform.forward) + (moveInput.x * transform.right) + (transform.up * -9.8f * Time.deltaTime));
+
+		Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		transform.Rotate(Vector3.up * mouseInput.x * MouseSensitivity);
+		xRotation -= mouseInput.y * MouseSensitivity;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+		CameraTransform.localRotation = Quaternion.Euler(new Vector3(xRotation, 0f, 0f));
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(CameraTransform.position, CameraTransform.forward, out hit))
+			{
+				Target t = hit.transform.GetComponent<Target>();
+				if(t)
+				{
+					t.Hit();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	private Vector3 moveTarget;
+	private Vector3 position;
+	private Vector3 velocity;
+
+	private void Start()
+	{
+		Hit();
+	}
+
+	private void Update()
+	{
+		transform.Rotate(new Vector3(10f, 20f, 30f) * Time.deltaTime);
+		transform.position = Vector3.SmoothDamp(transform.position, moveTarget, ref velocity, 0.25f);
+	}
+
+	public void Hit()
+	{
+		position = transform.position;
+		moveTarget = new Vector3(Random.Range(-10f, 10f), Random.Range(2.5f,6f), Random.Range(-12f, 21f));
+	}
+}
